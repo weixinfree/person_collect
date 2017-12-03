86b4f6d3a016d5643b340a417c40e29f15d016a1
BeyoundBoundary <weixinforeverlove@GMAIL.COM>
2017-12-03 09:42:50 +0800

    Create compress_png.sh

---
+++ scripts/compress_png.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+find . -name '*.png' | grep -v build | grep -v analyze | grep -v .9 | xargs -n1 file -F ' : ' | grep RGBA | awk '{print $1}' | xargs -n1 pngquant -f --skip-if-larger
+
+sum=0
+for file in `find . -name '*-fs8.png'`;
+do
+    dst=`echo $file | awk -F '-fs8' '{print $1.$2}'`
+
+    source_png_size=`ls -l $dst | awk '{printf "%.2f",$5/1024}'`
+    opti_png_size=`ls -l $file | awk '{printf "%.2f", $5/1024}'`
+
+    echo
+    echo 压缩${dst}...
+    percent=`echo "scale=2; ($source_png_size - $opti_png_size) / $source_png_size * 100" | bc -l`
+
+    echo 原始大小 $source_png_size KB, 优化后 $opti_png_size KB, 压缩百分比: $percent%
+    sum=`echo "scale=2; $sum + ${source_png_size} - ${opti_png_size}" | bc -l`
+
+    mv -f $file $dst
+done
+
+echo 共压缩: $sum KB
