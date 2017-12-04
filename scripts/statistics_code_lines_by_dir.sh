d6020eb9d3741f7c49c2815aa5484d2dc092bcfa
BeyoundBoundary <weixinforeverlove@GMAIL.COM>
2017-12-04 17:26:02 +0800

    Update statistics_code_lines_by_dir.sh

--- scripts/statistics_code_lines_by_dir.sh
+++ scripts/statistics_code_lines_by_dir.sh
@@ -1,1 +1,1 @@
-ls | while read name; do  echo -n "$name :  "; find $name -name '*.java' | xargs -n1 wc -l | awk '{sum+=$1}END{print sum}'; done
+ls | while read name; do  echo -n "$name :  "; find $name -name '*.java' | xargs -n1 wc -l | awk '{sum+=$1}END{print sum}'; done | sort -n -k 2 -t :
